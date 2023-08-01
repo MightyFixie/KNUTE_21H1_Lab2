614dfa33bb06ec579012358ee48dd054358d6f21
PoteznaSowa <64582266+PoteznaSowa@users.noreply.github.com>
2023-08-01 18:05:19 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 
 static class Program {
-	// Розширювальний метод для отримання масиву зі списку.
 	static T[] GetArray<T>(this MyList<T> list) {
 		T[] result = new T[list.Count];
 		for (int i = 0; i < list.Count; i++)
@@ -9,7 +8,6 @@ static class Program {
 		return result;
 	}
 
-	// Випадкові числа.
 	static readonly Random rng = MyClass.FactoryMethod<Random>();
 	static int GetRandomInt() => (rng.Next() << 1) ^ rng.Next();
 
@@ -17,18 +15,15 @@ static class Program {
 		Console.Title = "Лабораторна 2";
 		Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-		// Створити список і заповнити його випадковими числами.
 		MyList<int> list = new();
 		for (int i = rng.Next(22); i != 0; i--)
 			list.Add(GetRandomInt());
 
-		// Створити словник і заповнити його аргументами запуску програми.
 		MyDictionary<char, string> dictionary = new();
 		char c = 'A';
 		foreach (string s in args)
 			dictionary.Add(c++, s);
 
-		// Вивести зміст словника на екран.
 		Console.WriteLine($"Розмір словника: {dictionary.Count}");
 		c = 'A';
 		for (int i = 0; i < dictionary.Count; i++) {
@@ -36,7 +31,6 @@ static class Program {
 			c++;
 		}
 
-		// Вивести вміст списка в масив, а далі на екран.
 		Console.WriteLine($"Розмір списка: {list.Count}");
 		foreach (int i in list.GetArray())
 			Console.WriteLine(i);
